f089d413a620fdf9f537cbeb3194f812d4fe2865
Seunghun Lee <hun2456@hanmail.net>
2021-12-31 00:06:36 +0900

    RedirectAttributes

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -60,6 +61,15 @@ public class BasicItemController {
 
         model.addAttribute("item",item);
         return "basic/item";
+    }
+
+    @PostMapping("/add")
+    public String addItem6(Item item, RedirectAttributes redirectAttributes){
+
+       Item savedId = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedId.getId());
+        redirectAttributes.addAttribute("status",true);
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
